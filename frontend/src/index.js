3856c8eb1ea653fbe5b658c6c88852c36c513467
Eric Berk <eric.berk@web.de>
2026-02-28 15:23:05 +0100

    Füge Xirsys TURN-Integration und OnlineContext hinzu

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { VoiceProvider } from "./context/VoiceContext";
+import { OnlineProvider } from "./context/OnlineContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <VoiceProvider>
-      <App />
-    </VoiceProvider>
+    <OnlineProvider>
+      <VoiceProvider>
+        <App />
+      </VoiceProvider>
+    </OnlineProvider>
   </React.StrictMode>
 );
